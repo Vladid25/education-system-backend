c6d986cd522ecb0718696c60979ae221bc49893f
Vlad Vasiliev <v14d06@gmail.com>
2025-01-25 12:51:06 +0200

    Database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+
+config();
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: process.env.DB_PASSWORD,
+      username: 'postgres',
+      entities: [],
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: true,
+    }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
